f162a15660574c50a3b60d7e3d89336843a1248a
kowshikmeda <medakowshik8@gmail.com>
2025-11-16 21:39:05 +0530

    cors

--- BackEnd/video.js
+++ BackEnd/video.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 export const initSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: process.env.CLIENT_URL || "http://localhost:5173",
+      origin: [process.env.CLIENT_URL , "http://localhost:5173"],
       methods: ["GET", "POST"],
       credentials: true,
     },
